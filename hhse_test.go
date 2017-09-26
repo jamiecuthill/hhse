881262a5cf1dcd9473a3be01b5aa01509da017ea
Jamie Cuthill <jamiec@flypay.co.uk>
2017-09-26 16:41:26 +0100

    Implement incresing current price when receiving bill events

--- hhse_test.go
+++ hhse_test.go
@@ -47,14 +47,14 @@ var _ = Describe("Hhse", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(string(body)).To(MatchJSON(`{
-							"items": [
-								{ "id": 1, "name": "Stella" },
-								{ "id": 2, "name": "Carlsberg" },
-								{ "id": 3, "name": "Coors Light" },
-								{ "id": 4, "name": "Carling" },
-								{ "id": 5, "name": "Budweiser" }
-							]
-						}`))
+				"items": [
+					{ "id": 1, "name": "Stella" },
+					{ "id": 2, "name": "Carlsberg" },
+					{ "id": 3, "name": "Coors Light" },
+					{ "id": 4, "name": "Carling" },
+					{ "id": 5, "name": "Budweiser" }
+				]
+			}`))
 		})
 	})
 
@@ -71,14 +71,98 @@ var _ = Describe("Hhse", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(string(body)).To(MatchJSON(`{
-							"prices": [
-								{ "id": 1, "low": "£1.08", "high": "£1.08", "current": "£1.08", "trend": "" },
-								{ "id": 2, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" },
-								{ "id": 3, "low": "£0.84", "high": "£0.84", "current": "£0.84", "trend": "" },
-								{ "id": 4, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" },
-								{ "id": 5, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" }
-							]
-						}`))
+				"prices": [
+					{ "id": 1, "low": "£1.08", "high": "£1.08", "current": "£1.08", "trend": "" },
+					{ "id": 2, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" },
+					{ "id": 3, "low": "£0.84", "high": "£0.84", "current": "£0.84", "trend": "" },
+					{ "id": 4, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" },
+					{ "id": 5, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" }
+				]
+			}`))
+		})
+
+		Context("when a bill event indicates a sale", func() {
+			BeforeEach(func() {
+				resp, err := http.Post(endpoint("/events"), "application/json", strings.NewReader(`{
+					"checks": [{
+						"checkNumber": "34",
+						"createdAt": "2017-06-15 20:00:00",
+						"lastUpdated": "2017-06-15 20:00:00"
+					}],
+					"bill": {
+						"locationId": 123,
+						"openedAt": "2017-06-15 20:00:00",
+						"outstanding": 74.88,
+						"lastUpdated": null,
+						"closedAt": null,
+						"discount": 0,
+						"fullAmount": 2.16,
+						"table": {
+							"tableCode": "35",
+							"guestCount": 2
+						},
+						"id": 1,
+						"staff": [{
+							"staffCode": "3",
+							"name": null
+						}],
+						"serviceCharge": 0.00,
+						"tipsPaid": 0,
+						"products": [{
+							"category": {
+								"id": 1,
+								"name": "drinks",
+								"group": "drinks"
+							},
+							"price": 1.08,
+							"priceSold": 0,
+							"code": "42",
+							"flypayProductId": 1,
+							"productName": "Stella"
+						}, {
+							"category": {
+								"id": 1,
+								"name": "drinks",
+								"group": "drinks"
+							},
+							"price": 1.08,
+							"priceSold": 0,
+							"code": "42",
+							"flypayProductId": 1,
+							"productName": "Stella"
+						}],
+						"payments": [],
+						"type": "PayAtTable",
+						"vat": 0.00
+					}
+				}`))
+				Expect(err).NotTo(HaveOccurred())
+				defer resp.Body.Close()
+
+				Expect(resp.StatusCode).To(Equal(http.StatusNoContent))
+			})
+
+			It("price of the purchased item should increase", func() {
+				resp, err := http.Get(endpoint("/prices"))
+				Expect(err).NotTo(HaveOccurred())
+				defer resp.Body.Close()
+
+				Expect(resp.StatusCode).To(Equal(http.StatusOK))
+				Expect(resp.Header["Content-Type"]).To(ContainElement("application/json"))
+
+				body, err := ioutil.ReadAll(resp.Body)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(string(body)).To(MatchJSON(`{
+					"prices": [
+						{ "id": 1, "low": "£1.08", "high": "£1.18", "current": "£1.18", "trend": "up" },
+						{ "id": 2, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" },
+						{ "id": 3, "low": "£0.84", "high": "£0.84", "current": "£0.84", "trend": "" },
+						{ "id": 4, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" },
+						{ "id": 5, "low": "£0.96", "high": "£0.96", "current": "£0.96", "trend": "" }
+					]
+				}`))
+			})
 		})
 	})
 })
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"encoding/json"
 	"github.com/rs/cors"
+	"math"
 )
 
 const LowRatio = 0.2
@@ -15,15 +16,20 @@ const CrashRatio = 0.8
 const PriceIncrement = 0.04
 const ClockPeriodMinutes = 5
 
+const TrendUp = "up"
+
 type Menu struct {
-	Items []Product
+	Items []*Product
 }
 
 type Product struct {
-	ID    int
-	Name  string
-	Price int
-	Trend string
+	ID           int
+	Name         string
+	BasePrice    int
+	lowPrice     int
+	currentPrice int
+	highPrice    int
+	Trend        string
 }
 
 type itemResponse struct {
@@ -48,17 +54,29 @@ type pricesResponse struct {
 	Prices []priceResponse `json:"prices"`
 }
 
+type billEvent struct {
+	Bill billEventBill `json:"bill"`
+}
+
+type billEventBill struct {
+	Products []billEventProduct `json:"products"`
+}
+
+type billEventProduct struct {
+	ID int `json:"flypayProductId"`
+}
+
 var fakeMenu Menu
 
 func main() {
 
 	fakeMenu = Menu{
-		Items: []Product{
-			{ID: 1, Name: "Stella", Price: 540},
-			{ID: 2, Name: "Carlsberg", Price: 480},
-			{ID: 3, Name: "Coors Light", Price: 420},
-			{ID: 4, Name: "Carling", Price: 480},
-			{ID: 5, Name: "Budweiser", Price: 480},
+		Items: []*Product{
+			NewProduct(1, "Stella", 540),
+			NewProduct(2, "Carlsberg", 480),
+			NewProduct(3, "Coors Light", 420),
+			NewProduct(4, "Carling", 480),
+			NewProduct(5, "Budweiser", 480),
 		},
 	}
 
@@ -85,12 +103,35 @@ func main() {
 		var p pricesResponse
 
 		for _, product := range fakeMenu.Items {
-			p.Prices = append(p.Prices, newPriceResp(product))
+			p.Prices = append(p.Prices, newPriceResp(*product))
 		}
 
 		w.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(p)
 	}).Methods(http.MethodGet)
+
+	r.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		var event billEvent
+		err := json.NewDecoder(r.Body).Decode(&event)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		for _, product := range event.Bill.Products {
+			menuProduct, err := fakeMenu.Product(product.ID)
+			if err != nil {
+				continue
+			}
+
+			log.Printf("Found product, price: %v", menuProduct.currentPrice)
+			menuProduct.IncrPrice()
+			log.Printf("Incr product, price: %v", menuProduct.currentPrice)
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	})
 
 	c := cors.AllowAll()
 
@@ -98,6 +139,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func NewProduct(ID int, name string, price int) *Product {
+	initialPrice := int(float64(price) * LowRatio)
+	return &Product{
+		ID:           ID,
+		Name:         name,
+		BasePrice:    price,
+		lowPrice:     initialPrice,
+		currentPrice: initialPrice,
+		highPrice:    initialPrice,
+	}
+}
+
+func (product *Product) IncrPrice() {
+	product.currentPrice = int(math.Ceil(float64(product.currentPrice) * (1.0 + PriceIncrement)))
+
+	if (product.currentPrice > product.highPrice) {
+		product.highPrice = product.currentPrice
+	}
+
+	product.Trend = TrendUp
+}
+
+func (menu Menu) Product(productID int) (*Product, error) {
+	for _, product := range menu.Items {
+		if product.ID == productID {
+			return product, nil
+		}
+	}
+
+	return nil, fmt.Errorf("product %d not found", productID)
 }
 
 func newPriceResp(product Product) priceResponse {
@@ -111,15 +184,15 @@ func newPriceResp(product Product) priceResponse {
 }
 
 func (product Product) Current() int {
-	return product.Low()
+	return product.currentPrice
 }
 
 func (product Product) High() int {
-	return product.Low()
+	return product.highPrice
 }
 
 func (product Product) Low() int {
-	return int(float64(product.Price) * LowRatio)
+	return product.lowPrice
 }
 
 func toMoney(amount int) string {
