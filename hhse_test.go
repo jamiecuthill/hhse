d7b23436efcee14aa37ac12126472854cf672edb
Jamie Cuthill <jamiec@flypay.co.uk>
2017-09-26 13:08:21 +0100

    Add prices endpoint

--- hhse_test.go
+++ hhse_test.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 var _ = Describe("Hhse", func() {
 	Describe("Healthcheck", func() {
 		It("should respond OK", func() {
-			resp, err := http.Get(fmt.Sprintf("http://%s:%d", host, port))
+			resp, err := http.Get(endpoint("/"))
 			Expect(err).NotTo(HaveOccurred())
 			defer resp.Body.Close()
 
@@ -21,7 +22,7 @@ var _ = Describe("Hhse", func() {
 
 	Describe("Menu", func() {
 		It("should respond with menu", func() {
-			resp, err := http.Get(fmt.Sprintf("http://%s:%d/menu", host, port))
+			resp, err := http.Get(endpoint("/menu"))
 			Expect(err).NotTo(HaveOccurred())
 			defer resp.Body.Close()
 
@@ -32,14 +33,43 @@ var _ = Describe("Hhse", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(string(body)).To(MatchJSON(`{
-				"items": [
-					{ "name": "Stella" },
-					{ "name": "Carlsberg" },
-					{ "name": "Coors Light" },
-					{ "name": "Carling" },
-					{ "name": "Budweiser" }
-				]
-			}`))
+							"items": [
+								{ "id": 1, "name": "Stella" },
+								{ "id": 2, "name": "Carlsberg" },
+								{ "id": 3, "name": "Coors Light" },
+								{ "id": 4, "name": "Carling" },
+								{ "id": 5, "name": "Budweiser" }
+							]
+						}`))
+		})
+	})
+
+	Describe("Prices", func() {
+		It("should start up with default prices", func() {
+			resp, err := http.Get(endpoint("/prices"))
+			Expect(err).NotTo(HaveOccurred())
+			defer resp.Body.Close()
+
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+			Expect(resp.Header["Content-Type"]).To(ContainElement("application/json"))
+
+			body, err := ioutil.ReadAll(resp.Body)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(string(body)).To(MatchJSON(`{
+							"prices": [
+								{ "id": 1, "low": "£1.08", "high": "£5.40", "current": "£1.08", "trend": "" },
+								{ "id": 2, "low": "£0.96", "high": "£4.80", "current": "£0.96", "trend": "" },
+								{ "id": 3, "low": "£0.84", "high": "£4.20", "current": "£0.84", "trend": "" },
+								{ "id": 4, "low": "£0.96", "high": "£4.80", "current": "£0.96", "trend": "" },
+								{ "id": 5, "low": "£0.96", "high": "£4.80", "current": "£0.96", "trend": "" }
+							]
+						}`))
 		})
 	})
 })
+
+func endpoint(path string) string {
+	path = strings.TrimLeft(path, "/")
+	return fmt.Sprintf("http://%s:%d/%s", host, port, path)
+}
--- main.go
+++ main.go
@@ -9,37 +9,114 @@ import (
 	"encoding/json"
 )
 
-type item struct{
-	Name string `json:"name"`
+const LowRatio = 0.2
+const HighRatio = 0.8
+
+type Menu struct {
+	Items []Product
 }
 
-type menu struct{
-	Items []item `json:"items"`
+type Product struct {
+	ID    int
+	Name  string
+	Price int
+	Trend string
 }
+
+type itemResponse struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	price int
+}
+
+type menuResponse struct {
+	Items []itemResponse `json:"items"`
+}
+
+type priceResponse struct {
+	ID      int    `json:"id"`
+	Low     string `json:"low"`
+	High    string `json:"high"`
+	Current string `json:"current"`
+	Trend   string `json:"trend"`
+}
+
+type pricesResponse struct {
+	Prices []priceResponse `json:"prices"`
+}
+
+var fakeMenu Menu
 
 func main() {
+
+	fakeMenu = Menu{
+		Items: []Product{
+			{ID: 1, Name: "Stella", Price: 540},
+			{ID: 2, Name: "Carlsberg", Price: 480},
+			{ID: 3, Name: "Coors Light", Price: 420},
+			{ID: 4, Name: "Carling", Price: 480},
+			{ID: 5, Name: "Budweiser", Price: 480},
+		},
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
 	r.HandleFunc("/menu", func(w http.ResponseWriter, r *http.Request) {
-		menu := menu{
-			Items: []item{
-				{Name:"Stella"},
-				{Name:"Carlsberg"},
-				{Name:"Coors Light"},
-				{Name:"Carling"},
-				{Name:"Budweiser"},
-			},
+		var m menuResponse
+
+		for _, product := range fakeMenu.Items {
+			m.Items = append(m.Items, itemResponse{
+				ID: product.ID,
+				Name: product.Name,
+			})
 		}
 
 		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(menu)
+		json.NewEncoder(w).Encode(m)
+	})
+
+	r.HandleFunc("/prices", func(w http.ResponseWriter, r *http.Request) {
+		var p pricesResponse
+
+		for _, product := range fakeMenu.Items {
+			p.Prices = append(p.Prices, newPriceResp(product))
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(p)
 	})
 
 	err := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), r)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func newPriceResp(product Product) priceResponse {
+	return priceResponse{
+		ID:      product.ID,
+		Low:     toMoney(product.Low()),
+		High:    toMoney(product.High()),
+		Current: toMoney(product.Current()),
+		Trend:   product.Trend,
+	}
+}
+
+func (product Product) Current() int {
+	return product.Low()
+}
+
+func (product Product) High() int {
+	return product.Price
+}
+
+func (product Product) Low() int {
+	return int(float64(product.Price) * LowRatio)
+}
+
+func toMoney(amount int) string {
+	return fmt.Sprintf("£%.2f", float64(amount)/100.0)
 }
